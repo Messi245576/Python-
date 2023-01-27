2069bc30447d2f93665d13c2c47622e6bb80a351
Tanishk Shinde <97602467+Messi245576@users.noreply.github.com>
2023-01-27 13:56:29 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,45 @@
+print(15+6)
+
+a=7
+b=9
+print(a+b)
+print(a-b)
+print(a/b)
+print(a*b)
+print(a//b)
+print(a%b)
+mee=a+b
+print("ans is amuu",a,"add",b,mee)
+
+
+
+print(15-6)
+print(15*6)
+print(15/6)
+print(15//6)
+
+
+num1 = float(input("Please input your first number: "))
+op = input("Input the operation you would like to use: ")
+num2 = float(input("Please input your second number: "))
+if op == "+":
+  print(num1 + num2)
+elif op == "-":
+  print(num1 - num2)
+elif op == "*":
+  print(num1 * num2)
+elif op == "/":
+  print(num1/num2)
+else:
+  print("Invalid operator")
+
+number1=int(input("messo"))
+op=input("messsi")
+number2=int(input("messiii"))
+if op=="+":
+    print(number1+number2)
+elif op=="-":
+    print(number1-number2)
+elif op=="*":
+    print(number1*number2)    
+    
